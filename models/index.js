83380f7b6f131f90d0f319a635718339d1d664b0
WNicoleS <133277288+WNicoleS@users.noreply.github.com>
2023-11-12 22:33:08 -0600

    Removed routers

--- models/index.js
+++ models/index.js
@@ -1,11 +1,4 @@
-const router = require('express').Router();
 const User = require('./user');
 const Thoughts = require('./thoughts');
-
-//router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-  return res.send('Wrong route!');
-});
 
 module.exports = { User, Thoughts };
